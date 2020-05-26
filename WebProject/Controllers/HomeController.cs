13c7c79aa9f6b857cfeff3860b17e29ada5e25f0
Ahmet Murat Ilgun <muratilgun34@hotmail.com>
2020-05-26 11:06:23 +0300

    anasayfa düzenlemeleri yapıldı footer ve hakkımızda sayfaları yapılıyor

--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebProject.Models.DataContext;
 
 namespace WebProject.Controllers
 {
     
     public class HomeController : Controller
     {
+        private CorporateDBContext db = new CorporateDBContext();
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Service = db.Services.ToList().OrderByDescending(x => x.ServiceId);
+            ViewBag.Contacts = db.Contacts.SingleOrDefault();
+            ViewBag.Blog = db.Blogs.ToList().OrderByDescending(x => x.BlogId);
             return View();
         }
+
+        public ActionResult SliderPartial()
+        {
+
+            return View(db.Sliders.ToList().OrderByDescending(x=> x.SliderId));
+        }
+        public ActionResult ServicePartial()
+        {
+
+            return View(db.Services.ToList().OrderByDescending(x => x.ServiceId));
+        }
+
+        public ActionResult About()
+        {
+
+            return View(db.Abouts.SingleOrDefault());
+        }
+
     }
 }
